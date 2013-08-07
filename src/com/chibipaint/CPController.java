4192de51421a9b41bf1596ce602b61ae7b727df1
JLChnToZ <jeremylam619@gmail.com>
2013-08-07 14:49:19 +0800

    Remove unused dialog callers.

--- src/com/chibipaint/CPController.java
+++ src/com/chibipaint/CPController.java
@@ -293,55 +293,6 @@ public abstract class CPController {
 		return artwork;
 	}
 
-	//
-	// misc dialog boxes that shouldn't be here v___v
-
-	public void showBoxBlurDialog() {
-		/*JPanel panel = new JPanel();
-
-		panel.add(new JLabel("Blur amount:"));
-		SpinnerModel blurXSM = new SpinnerNumberModel(3, 1, 100, 1);
-		JSpinner blurX = new JSpinner(blurXSM);
-		panel.add(blurX);
-
-		panel.add(new JLabel("Iterations:"));
-		SpinnerModel iterSM = new SpinnerNumberModel(1, 1, 8, 1);
-		JSpinner iter = new JSpinner(iterSM);
-		panel.add(iter);
-
-		Object[] array = { "Box blur\n\n", panel };
-		int choice = JOptionPane.showConfirmDialog(getDialogParent(), array, "Box Blur", JOptionPane.OK_CANCEL_OPTION,
-				JOptionPane.PLAIN_MESSAGE);
-
-		if (choice == JOptionPane.OK_OPTION) {
-			int blur = ((Integer) blurX.getValue()).intValue();
-			int iterations = ((Integer) iter.getValue()).intValue();
-
-			artwork.boxBlur(blur, blur, iterations);
-			canvas.invalidate();
-		}*/
-	}
-
-	public void showGridOptionsDialog() {
-		/*JPanel panel = new JPanel();
-
-		panel.add(new JLabel("Grid Size:"));
-		SpinnerModel sizeSM = new SpinnerNumberModel(canvas.gridSize, 1, 1000, 1);
-		JSpinner sizeSpinner = new JSpinner(sizeSM);
-		panel.add(sizeSpinner);
-
-		Object[] array = { "Grid Options\n\n", panel };
-		int choice = JOptionPane.showConfirmDialog(getDialogParent(), array, "Grid Options",
-				JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-		if (choice == JOptionPane.OK_OPTION) {
-			int size = ((Integer) sizeSpinner.getValue()).intValue();
-
-			canvas.gridSize = size;
-			canvas.invalidate();
-		}*/
-
-	}
-
 	public boolean isRunningAsApplet() {
 		return false;
 	}
